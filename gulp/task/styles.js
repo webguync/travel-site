4dbeda4375ecd91691fff5c901f98b67d4f09071
Bruce <webguync@gmail.com>
2017-02-19 16:15:59 -0500

    made a few new changes

--- gulp/task/styles.js
+++ gulp/task/styles.js
@@ -7,13 +7,12 @@ cssImport = require('postcss-import'),
 mixins = require('postcss-mixins'),
 hexrgba = require('postcss-hexrgba');
 
-
 gulp.task('styles', function() {
- return gulp.src('./app/assets/styles/styles.css')
- .pipe(postcss([cssImport, mixins,cssvars,nested,hexrgba,autoprefixer]))
- .on('error', function(errorInfo){
-	 console.log(errorInfo.toString());
-	 this.emit('end');
- })
- .pipe(gulp.dest('./app/temp/styles'));
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
 });
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@
 require ('./gulp/task/styles');
 require ('./gulp/task/watch');
 require ('./gulp/task/scripts');
-
+require ('./gulp/task/sprites');
+require ('./gulp/task/modernizr');
+require ('./gulp/task/build');
 
 
 
